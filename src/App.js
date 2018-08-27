e36ed0b6ca20eaf940aac4a01854681e87d1d03a
sersoong <sersoong@gmail.com>
2018-08-27 12:52:39 +0800

    添加react-app-rewired

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Layout,Menu,Breadcrumb,Icon } from "antd";
+
 import './App.css';
+import Sider from 'antd/lib/layout/Sider';
+import {  } from "antd/lib/style/css";
+import SubMenu from 'antd/lib/menu/SubMenu';
+import MenuItem from 'antd/lib/menu/MenuItem';
+
+const { Header,Content,Footer } = Layout
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <Layout>
+        <Header>
+          <Breadcrumb>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          </Breadcrumb>
+          
+          </Header>
+        <Layout>
+          <Sider>left sidebar</Sider>
+          <Content>main Content</Content>
+          <Sider>right sidebar</Sider>
+        </Layout>
+        <Footer>footer</Footer>
+       </Layout>
       </div>
     );
   }
