ee35b343499d080f429e461ae5c0335bf7efef87
sersoong <sersoong@gmail.com>
2018-08-31 10:35:19 +0800

    删除测试数据

--- src/Containers/Posts/index.js
+++ src/Containers/Posts/index.js
@@ -13,22 +13,6 @@ class Posts extends Component{
 
     render(){
         // console.log(this.props.article_list)
-        const listData = [];
-        for (let i = 0; i < 23; i++) {
-        listData.push({
-            href: 'http://ant.design',
-            title: `ant design part ${i}`,
-            thumb: '/static/02.png',
-            date: '2018/08/30',
-            like: '39',
-            messages: '3',
-            tags:['foo','bar'],
-            avatar: '/static/02.png',
-            description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-            content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-        });
-        }
-          
         return(
             <div className="row">
                 <Row  type="flex" justify="center" align="middle" >
